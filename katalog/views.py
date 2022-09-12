2865db7ef225e990853e7b584904f9619011d442
Raudhatul Jannah <aurarj18@gmail.com>
2022-09-12 23:15:57 +0700

    done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from multiprocessing import context
+from katalog.models import CatalogItem
+
+# TODO: Create your views here.
+def show_katalog(req):
+    data_katalog = CatalogItem.objects.all()
+    ctx = {
+        'list_katalog':  data_katalog,
+        'nama': 'Raudhatul Jannah',
+        'id'  : '2106654422'
+
+    }
+
+    return render(req, "katalog.html", ctx)
